599d21edc44882a94ece97186c6520c9c524665d
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-14 19:23:14 +0100

    Add cli.js to test database connection and print blogs in db

---
+++ src/cli.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const { Sequelize, QueryTypes } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const printBlogs = async () => {
+  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT });
+
+  blogs.forEach(({ author, title, likes }) => {
+    console.log(`${author || "Some author"}: '${title}', ${likes} likes`);
+  });
+
+  process.exit(1);
+};
+
+const init = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    await printBlogs();
+  } catch (error) {
+    console.error("Something went wrong: ", error);
+  } finally {
+    sequelize.close();
+  }
+};
+
+init();
