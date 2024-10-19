ede328c815a77d2f19a21f74d4cdb5d4ab1f2e32
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-19 18:49:47 +0100

    Add year column to blogs

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -2,6 +2,8 @@ const { Model, DataTypes } = require("sequelize");
 const { sequelize } = require("../utils/db");
 class Blog extends Model {}
 
+const yearMin = 1991;
+
 Blog.init(
   {
     id: {
@@ -23,6 +25,20 @@ Blog.init(
     likes: {
       type: DataTypes.INTEGER,
       defaultValue: 0,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        max: {
+          args: new Date().getFullYear(),
+          msg: "Year must be less than or equal to the current year",
+        },
+        min: {
+          args: yearMin,
+          msg: `Year must be greater than or equal to ${yearMin}`,
+        },
+      },
     },
   },
   {
