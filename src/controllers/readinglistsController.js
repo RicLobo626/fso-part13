2c20f50129ea92637b6ba145dd4e11027c1297f1
Ricardo Lobo <101189711+RicLobo626@users.noreply.github.com>
2024-10-21 02:28:58 +0100

    Remove unused log

--- src/controllers/readinglistsController.js
+++ src/controllers/readinglistsController.js
@@ -1,7 +1,6 @@
 const service = require("../services/readinglistService");
 
 const addBlogToReadingList = async (req, res) => {
-  console.log(req.body);
   const addedEntry = await service.addBlogToReadingList(req.body);
 
   res.status(201).json(addedEntry);
